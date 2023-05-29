df21d0e81771a8b49c80d275a0967849813143de
Burcu Deniz <dnzzburcu@gmail.com>
2023-05-29 23:19:01 +0300

    proje tamamlandı.testler hatasız.

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,9 +6,21 @@
     "message": "Geçemezsiniz!"
   }
 */
-function sinirli() {
 
-}
+const userModel = require("../users/users-model");
+const bcryptjs = require("bcryptjs");
+
+function sinirli(req, res, next) {
+  try {
+    if (req.session && req.session.user_id > 0) {
+      next();
+    } else {
+      res.status(401).json({ message: "Geçemezsiniz!" });
+    }
+  } catch (error) {
+    next(error);
+  }
+} //session'ı kontrol eder.
 
 /*
   req.body de verilen username halihazırda veritabanında varsa
@@ -18,8 +30,22 @@ function sinirli() {
     "message": "Username kullaniliyor"
   }
 */
-function usernameBostami() {
 
+// register aşamasında kullanılır. username önceden kullanılmış mı diye kontrol eder.
+
+async function usernameBostami(req, res, next) {
+  try {
+    let { username } = req.body;
+    const isExist = await userModel.goreBul({ username: username });
+    // dizi döner
+    if (isExist && isExist.length > 0) {
+      res.status(422).json({ message: "Username kullaniliyor" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 /*
@@ -30,8 +56,34 @@ function usernameBostami() {
     "message": "Geçersiz kriter"
   }
 */
-function usernameVarmi() {
 
+// login aşamasında kullanılır. requestteki username var mı diye bakar. Varsa req.body.password ile veritabanındaki parolalar eşleşiyor mu diye bakar.
+async function usernameVarmi(req, res, next) {
+  try {
+    let { username } = req.body;
+    const isExist = await userModel.goreBul({ username: username });
+    if (isExist && isExist.length > 0) {
+      let user = isExist[0];
+      let isPasswordMatch = bcryptjs.compareSync(
+        req.body.password,
+        user.password
+      );
+      if (isPasswordMatch) {
+        req.dbUser = user;
+        next();
+      } else {
+        res.status(401).json({
+          message: "Geçersiz kriter",
+        });
+      }
+    } else {
+      res.status(401).json({
+        message: "Geçersiz kriter",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 /*
@@ -42,8 +94,42 @@ function usernameVarmi() {
     "message": "Şifre 3 karakterden fazla olmalı"
   }
 */
-function sifreGecerlimi() {
 
+//Login ve register aşamalrında kullanılır.
+//Şifreyi alır ve karakter sayısını kontrol eder.
+function sifreGecerlimi(req, res, next) {
+  try {
+    let { password } = req.body;
+    if (!password || password.length < 3) {
+      res.status(422).json({ message: "Şifre 3 karakterden fazla olmalı" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
+
+//Login ve register aşamalrında kullanılır. username ve parola alınıp,varlığı kontrol edilir.
+function checkLoginPayload(req, res, next) {
+  try {
+    let { username, password } = req.body;
+    if (!username || !password) {
+      res.status(422).json({ message: "Şifre 3 karakterden fazla olmalı" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  sinirli,
+  usernameBostami,
+  usernameVarmi,
+  sifreGecerlimi,
+  checkLoginPayload,
+};
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,38 @@
 /**
   tüm kullanıcıları içeren bir DİZİ ye çözümlenir, tüm kullanıcılar { user_id, username } içerir
  */
-function bul() {
+const db = require("../../data/db-config");
 
+function bul() {
+  return db("users");
 }
 
 /**
   verilen filtreye sahip tüm kullanıcıları içeren bir DİZİ ye çözümlenir
  */
-function goreBul(filtre) {
 
+function goreBul(filtre) {
+  return db("users").where(filtre);
 }
 
 /**
   verilen user_id li kullanıcıya çözümlenir, kullanıcı { user_id, username } içerir
  */
-function idyeGoreBul(user_id) {
-
+async function idyeGoreBul(user_id) {
+  let arrayData = await db("users").where("user_id", user_id);
+  let objData = await db("users").where("user_id", user_id).first();
+  return objData;
 }
 
 /**
   yeni eklenen kullanıcıya çözümlenir { user_id, username }
  */
-function ekle(user) {
-
+async function ekle(user) {
+  let [user_id] = await db("users").insert(user); 
+  return idyeGoreBul(user_id);
 }
 
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
+
+
+module.exports={bul,goreBul,idyeGoreBul,ekle}
